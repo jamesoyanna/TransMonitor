de61f3040a28d371cd5fa16030add6d0b51164ca
eshoiza <engrjayt200@gmail.com>
2020-02-11 00:21:31 +0100

    added TransactionTable component

--- src/routes.js
+++ src/routes.js
@@ -8,8 +8,8 @@ import  DefaultLayout from "../src/layouts/Default";
 import TransactionOverview from "./views/TransactionOverview";
 import UserProfile from "./views/UserProfile";
 import AddTransaction from "./views/AddTransaction";
-import ComponentsOverview from "./views/AddTransaction";
-import TransactionTable from "./views/TransactionTable";
+//import ComponentsOverview from ".";
+import TransactionTable from "../src/views/TransactionTable";
 
 
 export default [
---
+++ src/views/TransactionTable.js
@@ -0,0 +1,115 @@
+import React from "react";
+import { Container, Row, Col, Card, CardHeader, CardBody } from "shards-react";
+
+import PageTitle from "../../components/common/PageTitle";
+import PaymentNavbar from "../../components/layout/MainNavbar/NavbarNav/PaymentNavbar";
+
+const TransactionTable = () => (
+  <Container fluid className="main-content-container px-4">
+    {/* Page Header */}
+    <Row noGutters className="page-header py-4">
+      <PageTitle sm="4" title="Payments" className="text-sm-left" />
+    </Row>
+
+    <Row>
+      <PaymentNavbar />
+    </Row>
+
+    {/* Transaction table*/}
+    <Row>
+      <Col>
+        <Card small className="mb-4">
+          <CardHeader className="border-bottom">
+            <p className="m-0">Showing</p>
+          </CardHeader>
+
+          <CardBody className="p-0 pb-3">
+            <table className="table mb-0">
+              <thead className="bg-light">
+                <tr>
+                  <th scope="col" className="border-0">
+                    Item Type
+                  </th>
+
+                  <th scope="col" className="border-0">
+                    Price
+                  </th>
+                  <th scope="col" className="border-0">
+                    Transaction No
+                  </th>
+                  <th scope="col" className="border-0">
+                    Time
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>Apple Mac Book 15" 250 SSD 12GB</td>
+                  <td>$73430</td>
+                  <td>Gdańsk</td>
+                  <td>12:30</td>
+                </tr>
+                <tr>
+                  <td>Apple Mac Book 15" 250 SSD 12GB</td>
+                  <td>$73430</td>
+                  <td>1234567890</td>
+                  <td>12:30</td>
+                </tr>
+                <tr>
+                  <td>Apple Mac Book 15" 250 SSD 12GB</td>
+                  <td>$73430</td>
+                  <td>1234567890</td>
+                  <td>12:30</td>
+                </tr>
+                <tr>
+                  <td>Apple Mac Book 15" 250 SSD 12GB</td>
+                  <td>$73430</td>
+                  <td>1234567890</td>
+                  <td>12:30</td>
+                </tr>
+                <tr>
+                  <td>Apple Mac Book 15" 250 SSD 12GB</td>
+                  <td>$73430</td>
+                  <td>1234567890</td>
+                  <td>12:30</td>
+                </tr>
+                <tr>
+                  <td>Apple Mac Book 15" 250 SSD 12GB</td>
+                  <td>$73430</td>
+                  <td>1234567890</td>
+                  <td>12:30</td>
+                </tr>
+                <tr>
+                  <td>Apple Mac Book 15" 250 SSD 12GB</td>
+                  <td>$73430</td>
+                  <td>1234567890</td>
+                  <td>12:30</td>
+                </tr>
+                <tr>
+                  <td>Apple Mac Book 15" 250 SSD 12GB</td>
+                  <td>$73430</td>
+                  <td>1234567890</td>
+                  <td>12:30</td>
+                </tr>
+                <tr>
+                  <td>Apple Mac Book 15" 250 SSD 12GB</td>
+                  <td>$73430</td>
+                  <td>1234567890</td>
+                  <td>12:30</td>
+                </tr>
+                <tr>
+                  <td>Apple Mac Book 15" 250 SSD 12GB</td>
+                  <td>$73430</td>
+                  <td>1234567890</td>
+                  <td>12:30</td>
+                </tr>
+              </tbody>
+            </table>
+          </CardBody>
+        </Card>
+      </Col>
+    </Row>
+  </Container>
+);
+
+export default TransactionTable;
