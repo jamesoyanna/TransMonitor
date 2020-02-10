a8eb73c78ee7b5efd7cd24454ed5eda445c3dfbf
eshoiza <engrjayt200@gmail.com>
2020-02-10 23:45:01 +0100

    added Support component

---
+++ components/layout/MainNavbar/NavbarNav/Support.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { NavItem, NavLink } from "shards-react";
+class Notifications extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      visible: false
+    };
+
+    this.toggleNotifications = this.toggleNotifications.bind(this);
+  }
+
+  toggleNotifications() {
+    this.setState({
+      visible: !this.state.visible
+    });
+  }
+
+  render() {
+    return (
+      <NavItem className="">
+        <NavLink className="" onClick={this.toggleNotifications}>
+          <p>Support</p>
+        </NavLink>
+      </NavItem>
+    );
+  }
+}
+
+export default Notifications;
