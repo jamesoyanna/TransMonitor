d4ea936d6d3955e5b0b2ac07fb5bc085da3ec071
eshoiza <engrjayt200@gmail.com>
2020-02-10 21:58:01 +0100

    added MainFooter

--- src/layouts/Default.js
+++ src/layouts/Default.js
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Container, Row, Col } from "shards-react";
 
-import MainNavbar from "../components/layout/MainNavbar/MainNavbar";
+import MainNavbar from "../../src/components/layout/MainNavbar/MainNavbar";
 import MainSidebar from "../components/layout/MainSidebar/MainSidebar";
 import MainFooter from "../components/layout/MainFooter";
 
