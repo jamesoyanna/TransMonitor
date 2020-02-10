9eadcfdb48e1bd1a6b5baa8deb023140cbb58982
eshoiza <engrjayt200@gmail.com>
2020-02-10 11:17:02 +0100

    added routes

---
+++ src/routes.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { Redirect } from "react-router-dom";
+
+// Layout Types
+import { DefaultLayout } from "./layouts";
+
+// Route Views
+import TransactionOverview from "./views/TransactionOverview";
+import UserProfile from "./views/UserProfile";
+import AddTransaction from "./views/AddTransaction";
+import ComponentsOverview from "./views/AddTransaction";
+import TransactionTable from "./views/TransactionTable";
+import Transaction from "./Transaction";
+
+export default [
+  {
+    path: "/",
+    exact: true,
+    layout: DefaultLayout,
+    component: () => <Redirect to="/trans-overview" />
+  },
+  {
+    path: "/trans-overview",
+    layout: DefaultLayout,
+    component: TransactionOverview
+  },
+  {
+    path: "/user-profile",
+    layout: DefaultLayout,
+    component: UserProfile
+  },
+  {
+    path: "/add-transaction",
+    layout: DefaultLayout,
+    component: AddTransaction
+  },
+ 
+  {
+    path: "/components-overview",
+    layout: DefaultLayout,
+    component: ComponentsOverview
+  },
+  {
+    path: "/transaction-table",
+    layout: DefaultLayout,
+    component: TransactionTable
+  },
+  {
+    path: "/transaction",
+    layout: DefaultLayout,
+    component: Transaction
+  }
+];
