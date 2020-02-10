5e9675e77344607244abf428cde84d52cad62a78
eshoiza <engrjayt200@gmail.com>
2020-02-10 12:48:59 +0100

    created AddTransactioncomponent

--- views/AddTransaction.js
+++ views/AddTransaction.js
@@ -2,9 +2,9 @@ import React from "react";
 import { Container, Row, Col } from "shards-react";
 
 import PageTitle from "../components/common/PageTitle";
-import Editor from "../components/add-new-post/Editor";
-import SidebarActions from "../components/add-new-post/SidebarActions";
-import SidebarCategories from "../components/add-new-post/SidebarCategories";
+import Editor from "../components/add-transaction/Editor";
+import SidebarActions from "../components/add-transaction/SidebarActions";
+import SidebarCategories from "../components/add-transaction/SidebarCategories";
 
 const AddTransaction = () => (
   <Container fluid className="main-content-container px-4 pb-4">
