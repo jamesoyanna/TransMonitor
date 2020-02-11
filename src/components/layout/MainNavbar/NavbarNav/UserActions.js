99e204d72ede2bb33604e6b03751900ea47254fa
eshoiza <engrjayt200@gmail.com>
2020-02-11 02:48:58 +0100

    updated userProfile component

--- src/components/layout/MainNavbar/NavbarNav/UserActions.js
+++ src/components/layout/MainNavbar/NavbarNav/UserActions.js
@@ -33,7 +33,7 @@ export default class UserActions extends React.Component {
         <DropdownToggle caret tag={NavLink} className="text-nowrap px-3">
           <img
             className="user-avatar rounded-circle mr-2"
-            src={require("../../../../src/images/james.jpg")}
+            src={require("../../../../images/james.jpg")}
             alt="User Avatar"
           />{" "}
           <span className="d-none d-md-inline-block">Oluwaleke Ojo</span>
--- src/views/TransactionOverview.js
+++ src/views/TransactionOverview.js
@@ -2,13 +2,13 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Container, Row, Col } from "shards-react";
 
-import PageTitle from "../../components/common/PageTitle";
+import PageTitle from "../components/common/PageTitle";
 import Statistics from "../../components/common/Statistics";
 import UsersOverview from "../../components/general/UsersOverview";
 import ProgressBar from "../../components/components-overview/ProgressBars";
 import PaymentNavbar from "../../components/layout/MainNavbar/NavbarNav/PaymentNavbar";
 import PaymentInfo from "../../src/views/PaymentInfo";
-import Pagination from "../views/Pagination";
+import Pagination from "../../src/views/Pagination";
 const TransactionOverview = ({ smallStats }) => (
   <Container fluid className="main-content-container px-4">
     {/* Page Header */}
